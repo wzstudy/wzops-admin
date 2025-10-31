0f352de10e6dadad591a92ecfbf36d955e9885d8
王震 <>
2025-10-31 15:08:54 +0800

    界面功能 - 添加比特浏览器窗口管理页面

--- backend/wzops/api.py
+++ backend/wzops/api.py
@@ -4,6 +4,7 @@
 # @FileName: api.py
 # @Software: PyCharm
 from apps.demo.router import demo_router
+from apps.browser.router import browser_router
 from system.router import system_router
 from utils.fu_auth import GlobalAuth
 from utils.fu_ninja import FuNinjaAPI
@@ -26,3 +27,4 @@ def a(request, exc):
 api.add_router('/system/', system_router)
 api.add_router('/demo/', demo_router)
 api.add_router('/generator/', generator_router)
+api.add_router('/browser/', browser_router)
